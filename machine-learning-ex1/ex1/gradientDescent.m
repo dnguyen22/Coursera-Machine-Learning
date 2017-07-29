7e7f1a47af383ffd667fea63eeb4f4a556d1684d
dnguyen22 <don.nguyen22@gmail.com>
2017-07-29 15:44:09 -0500

    Implemented single variable gradient descent

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -20,8 +20,8 @@ for iter = 1:num_iters
 
 
 
-
-
+    delta = 1/m * X' * (X * theta - y);
+    theta = theta - alpha * delta;
 
     % ============================================================
 
