405a080b8eeeeb99fad8f58394e36cd01ebd7b3f
dnguyen22 <don.nguyen22@gmail.com>
2017-08-02 22:34:01 -0500

    Neural network forward propagation implementation

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -21,12 +21,15 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+% Add ones to the X data matrix
+a1 = [ones(m, 1) X]';
+a2 = sigmoid(Theta1*a1);
+a2 = [ones(m, 1) a2'];
+a3 = sigmoid(Theta2*a2');
 
+[M, p] = max(a3, [], 1);
 
-
-
-
-
+p = p';
 
 
 % =========================================================================
