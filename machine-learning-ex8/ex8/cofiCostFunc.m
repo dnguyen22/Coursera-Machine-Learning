d7004d7626df9ceadd99e58bd04cf82311bf12f6
dnguyen22 <don.nguyen22@gmail.com>
2017-08-18 22:12:03 -0500

    Calculate cost function without regularization

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -39,9 +39,16 @@ Theta_grad = zeros(size(Theta));
 %        Theta_grad - num_users x num_features matrix, containing the 
 %                     partial derivatives w.r.t. to each element of Theta
 %
+J = 0
+for i=1:num_movies
+    for j=1:num_users
+        if R(i, j) == 1
+            J = J + (X(i, :) * Theta(j, :)' - Y(i, j)) ^ 2;
+        end
+    end
+end
 
-
-
+J = J/2;
 
 
 
